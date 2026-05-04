7ad217075804d77cf75ddd7b6f0bbe04de8980c4
Natty <bilalvmlo@icloud.com>
2026-05-04 05:26:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+import asyncio
+import json
+import requests
+import websockets
+
+TOKEN = "Add your token here"
+GUILD_ID = "ADD_YOUR_SERVER_ID_HERE"
+CHANNEL_ID = "ADD_YOUR_CHANNEL_ID_HERE"
+
+STATUS = "online" # online / dnd / idle
+SELF_MUTE = True
+SELF_DEAF = False
+
+API = "https://discord.com/api/v10"
+
+res = requests.get(f"{API}/users/@me", headers={"Authorization": TOKEN})
+if res.status_code != 200:
+    print("Invalid token!")
+    exit()
+
+user = res.json()
+print(f"Logged in as {user['username']} ({user['id']})!")
+
+async def heartbeat(ws, interval):
+    while True:
+        await asyncio.sleep(interval / 1000)
+        await ws.send(json.dumps({"op": 1, "d": None}))
+
+async def main():
+    uri = "wss://gateway.discord.gg/?v=10&encoding=json"
+
+    async with websockets.connect(uri, max_size=10 * 1024 * 1024) as ws:
+        hello = json.loads(await ws.recv())
+        heartbeat_interval = hello["d"]["heartbeat_interval"]
+
+        asyncio.create_task(heartbeat(ws, heartbeat_interval))
+
+        await ws.send(json.dumps({
+            "op": 2,
+            "d": {
+                "token": TOKEN,
+                "properties": {
+                    "$os": "windows",
+                    "$browser": "chrome",
+                    "$device": "pc"
+                },
+                "presence": {
+                    "status": STATUS,
+                    "afk": False
+                }
+            }
+        }))
+
+        while True:
+            event = json.loads(await ws.recv())
+            if event.get("t") == "READY":
+                break
+
+        await ws.send(json.dumps({
+            "op": 4,
+            "d": {
+                "guild_id": GUILD_ID,
+                "channel_id": CHANNEL_ID,
+                "self_mute": SELF_MUTE,
+                "self_deaf": SELF_DEAF
+            }
+        }))
+
+        print("Joined the voice channel!")
+
+        while True:
+            try:
+                msg = await ws.recv()
+            except:
+                print("Disconnected, reconnecting...")
+                break
+
+async def run():
+    while True:
+        try:
+            await main()
+        except Exception as e:
+            print("Error: ", e)
+            await asyncio.sleep(5)
+
+asyncio.run(run())
